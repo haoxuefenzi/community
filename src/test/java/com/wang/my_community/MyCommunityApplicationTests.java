093990ace205ffc55d51b2b7f621b7fb7ba4ae17
wang_glb <wuhu_wang@126.com>
2020-03-14 01:17:48 +0800

    init repo

---
+++ src/test/java/com/wang/my_community/MyCommunityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wang.my_community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyCommunityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
